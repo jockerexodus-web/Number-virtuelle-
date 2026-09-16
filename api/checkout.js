f3b9869b238721567d8fcdefe79ab51a3c69086a
SAKAMOTO-TECH <jockerexodus@gmail.com>
2026-09-16 05:29:28 +0100

    Delete api directory

--- api/checkout.js
+++
@@ -1,5 +0,0 @@
-// Ce fichier n'est plus nécessaire puisque les achats se font via WhatsApp.
-// Tu peux le supprimer ou le garder vide pour plus tard.
-export default function handler(req, res) {
-  res.status(200).json({ message: 'Contact WhatsApp : +241 02 73 86 80' });
-}
